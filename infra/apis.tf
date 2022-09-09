d0dda08ed5fbbad35ac9b63368d1fe184e62f121
James Fricker <jimmyf98@hotmail.com>
2022-09-09 20:31:20 +1000

    Update apis.tf (#11)
    * Update apis.tf
    
    * revert commit
    
    * revert commit

--- infra/apis.tf
+++ infra/apis.tf
@@ -3,8 +3,8 @@ resource "google_project_service" "enabled_apis" {
   for_each = toset([
     "serviceusage.googleapis.com",
     "iam.googleapis.com",
-    "dns.googleapis.com",
     "cloudresourcemanager.googleapis.com",
+    "dns.googleapis.com",
     "cloudbuild.googleapis.com",
     "run.googleapis.com",
     "compute.googleapis.com"
