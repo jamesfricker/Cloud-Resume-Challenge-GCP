cd547f240796caa3fe4122fe75b5ae619f63941e
James Fricker <jimmyf98@hotmail.com>
2022-09-09 20:27:55 +1000

    revert commit

--- infra/main.tf
+++ infra/main.tf
@@ -3,12 +3,6 @@ provider "google" {
   region  = var.region
 }
 
-# GCP beta provider
-provider "google-beta" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_storage_bucket" "resume_bucket" {
   name          = var.resume_bucket
   location      = var.region
@@ -22,4 +16,12 @@ resource "google_storage_bucket" "resume_bucket" {
     response_header = ["*"]
     max_age_seconds = 3600
   }
+}
+
+
+# Make bucket public by granting allUsers READER access
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.resume_bucket.id
+  role   = "READER"
+  entity = "allUsers"
 }
